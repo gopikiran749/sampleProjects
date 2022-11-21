1094c8712273e2bc6976f77e61eec29d019267e9
gopikiran749 <gopikiran749@gmail.com>
2022-11-21 07:43:11 +0530

    Array projrct

---
+++ ArrayProject/src/ArrarsTest/arrays.java
@@ -0,0 +1,20 @@
+package ArrarsTest;
+
+public class arrays {
+
+	public static void main(String[] args) {
+		
+		
+		//integers counting
+		int[] marks={10,20,30,40,50};
+		System.out.println(marks.length);
+		System.out.println(marks[4]);
+		
+		//String counting
+		//find index value
+		String[] list = {"gopi","raju","vassu"};
+		System.out.println(list.length);
+		System.out.println(list[1]);
+	}
+
+}
